5ad0aa7dd7f5e4c05cd4c02831bcd2d6af2f4310
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2023-08-21 14:59:26 -0400

    Goal unit test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate syn;
+
 use syn::{visit::Visit, ItemFn};
 use std::fs;
 
@@ -15,9 +17,26 @@ impl <'ast> Visit<'ast> for FunctionVisitor {
 }
 
 fn search_in_file(file: &str) -> Vec<String> {
-    let content = fs::read_to_string(file).expect("Unable to read file");
+    let content = fs::read_to_string(file).expect("Unable to read file when searching.");
     let syntax_tree: syn::File = syn::parse_str(&content).expect("Unable to parse file");
     let mut visitor = FunctionVisitor { matches: Vec::new() };
     visitor.visit_file(&syntax_tree);
     visitor.matches
+}
+
+fn test(testing: usize) -> usize {
+    testing
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn visit_file() {
+        let matches = search_in_file("~/Projects/rahoo/src/lib.rs");
+        for signature in matches {
+            println!("{}", signature);
+        }
+        assert!(false);
+    }
 }
